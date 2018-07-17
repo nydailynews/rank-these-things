20a3212b7cac8fd0899314e63a5dd695db3ccb4e
Joe Murphy <joe.murphy@gmail.com>
2018-07-17 14:13:57 -0400

    Update items.sql

--- data/items.sql
+++ data/items.sql
@@ -1,9 +1,9 @@
 # Run the query on line two first.
 INSERT INTO ranker (slug, create_date) VALUES ('name-of-your-ranker', NOW());
 # After you run it, run the next query (the one on lne 5). You'll get a few records back,
-# you're interested in the value of the ranker_id field for the new record you created.
+# you're interested in the value of the id field for the new record you created.
 SELECT * FROM ranker;
-# Fine-tune this query next. Instead of the "2" that's on each line, use the ranker_id.
+# Fine-tune this query next. Instead of the "2" that's on each line, use the id for the ranker you created.
 INSERT INTO ranker_items (ranker_id, title) VALUES
 (2,'hi'),
 (2,'bye'),
